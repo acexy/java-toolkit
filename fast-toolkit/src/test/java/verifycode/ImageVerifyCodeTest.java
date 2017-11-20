14eb27be88d7c3f753e8ad12aad0c373ea0353d9
Acexy <zhaoxy@thankjava.com>
2017-11-20 19:11:01 +0800

    新增图片验证码模块

---
+++ fast-toolkit/src/test/java/verifycode/ImageVerifyCodeTest.java
@@ -0,0 +1,26 @@
+package verifycode;
+
+import com.thankjava.toolkit.verifycode.ImageVerifyCode;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 简易图片验证码
+ */
+public class ImageVerifyCodeTest {
+
+    public static void main(String[] args) throws IOException {
+
+        ImageVerifyCode.writeImage(200, 80, "f:/a.jpg", "1q2w34");
+
+        byte[] bytes = ImageVerifyCode.encodeImage(200,80,"1q2w3e");
+        ImageIO.write(ImageIO.read(new ByteArrayInputStream(bytes)),"png",new File("f:/b.jpg"));
+    }
+
+}
