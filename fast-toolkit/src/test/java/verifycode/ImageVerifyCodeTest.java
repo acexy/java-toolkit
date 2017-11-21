ae7f316fdb330e13101478e8c933403d306474e7
Acexy <zhaoxy@thankjava.com>
2017-11-21 13:28:30 +0800

    调整测试方法

--- fast-toolkit/src/test/java/verifycode/ImageVerifyCodeTest.java
+++ fast-toolkit/src/test/java/verifycode/ImageVerifyCodeTest.java
@@ -17,10 +17,10 @@ public class ImageVerifyCodeTest {
 
     public static void main(String[] args) throws IOException {
 
-        ImageVerifyCode.writeImage(200, 80, "f:/a.jpg", "1q2w34");
+        ImageVerifyCode.writeImage(120, 40, "f:/code.jpg", "2w34");
 
-        byte[] bytes = ImageVerifyCode.encodeImage(200,80,"1q2w3e");
-        ImageIO.write(ImageIO.read(new ByteArrayInputStream(bytes)),"png",new File("f:/b.jpg"));
+//        byte[] bytes = ImageVerifyCode.encodeImage(200,80,"1q2w3e");
+//        ImageIO.write(ImageIO.read(new ByteArrayInputStream(bytes)),"png",new File("f:/b.jpg"));
     }
 
 }
