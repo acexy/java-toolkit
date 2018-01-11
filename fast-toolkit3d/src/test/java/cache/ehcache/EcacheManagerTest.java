6f98d284099663d5e0870e2fb63dee2088182c43
Acexy <thankjava@foxmail.com>
2018-01-11 16:06:42 +0800

    修正拼写错误

--- fast-toolkit3d/src/test/java/cache/ehcache/EcacheManagerTest.java
+++ fast-toolkit3d/src/test/java/cache/ehcache/EcacheManagerTest.java
@@ -1,11 +1,11 @@
 package cache.ehcache;
 
-import com.thankjava.toolkit3d.cache.ehcache.EcacheManager;
+import com.thankjava.toolkit3d.cache.ehcache.EhcacheManager;
 
 public class EcacheManagerTest {
 
 	public static void main(String[] args) {
-		EcacheManager manager = new EcacheManager();
+		EhcacheManager manager = new EhcacheManager();
 		
 		manager.setCache("test","aa", "什么");
 		System.out.println(manager.getCache("test","aa"));
