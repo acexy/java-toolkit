14200dd2fdd5653d5a091b889a24d9b9959cab40
Acexy <thankjava@foxmail.com>
2018-04-16 15:53:05 +0800

    格式化代码结构

--- fast-toolkit/src/main/java/com/thankjava/toolkit/aop/core/AopProxyFactory.java
+++ fast-toolkit/src/main/java/com/thankjava/toolkit/aop/core/AopProxyFactory.java
@@ -4,32 +4,34 @@ import java.lang.reflect.Proxy;
 
 /**
  * 创建基于JDK的代理对象 JDK代理是基于接口层面的
-* <p>Function: AopProxyFactory</p>
-* <p>Description: </p>
-* @author acexy@thankjava.com
-* @date 2016年8月17日 下午1:56:28
-* @version 1.0
+ * <p>Function: AopProxyFactory</p>
+ * <p>Description: </p>
+ *
+ * @author acexy@thankjava.com
+ * @version 1.0
+ * @date 2016年8月17日 下午1:56:28
  */
 public class AopProxyFactory {
 
-	/**
-	 * 创建代理对象
-	* <p>Function: createProxyObject</p>
-	* <p>Description: </p>
-	* @author acexy@thankjava.com
-	* @date 2016年8月17日 下午1:58:11
-	* @version 1.0
-	* @param interfaceType	接口类类型
-	* @param implementObject	基于interfaceType接口的实现类实例
-	 * @return 
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T> T createProxyObject(Class<T> interfaceType,Object implementObject){
-		return (T)Proxy.newProxyInstance(
-				implementObject.getClass().getClassLoader(),
-				implementObject.getClass().getInterfaces(), 
-				new InvokeInterceptor(implementObject)
-				);
-		
-	}
+    /**
+     * 创建代理对象
+     * <p>Function: createProxyObject</p>
+     * <p>Description: </p>
+     *
+     * @param interfaceType   接口类类型
+     * @param implementObject 基于interfaceType接口的实现类实例
+     * @return
+     * @author acexy@thankjava.com
+     * @date 2016年8月17日 下午1:58:11
+     * @version 1.0
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T createProxyObject(Class<T> interfaceType, Object implementObject) {
+        return (T) Proxy.newProxyInstance(
+                implementObject.getClass().getClassLoader(),
+                implementObject.getClass().getInterfaces(),
+                new InvokeInterceptor(implementObject)
+        );
+
+    }
 }
