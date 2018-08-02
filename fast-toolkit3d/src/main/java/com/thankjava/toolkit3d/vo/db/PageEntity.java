4b4a2e46ecbf0d3519d1fd676d6d0d643297c3e8
Acexy <acexy@thankjava.com>
2018-08-02 09:34:39 +0800

    新增mongo分页查询插件

---
+++ fast-toolkit3d/src/main/java/com/thankjava/toolkit3d/vo/db/PageEntity.java
@@ -0,0 +1,105 @@
+package com.thankjava.toolkit3d.vo.db;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: acexy@thankjava.com
+ * 2018/8/1
+ * @Description:
+ **/
+public class PageEntity<T> implements Serializable {
+
+    private PageEntity() {
+    }
+
+    /**
+     * 创建分页对象
+     *
+     * @param tClass         返回数据的javaBean class 类型指定
+     * @param queryCondition
+     * @return
+     */
+    public static PageEntity newPageEntity(Class<?> tClass, Object queryCondition) {
+        PageEntity pageEntity = new PageEntity();
+        pageEntity.queryCondition = queryCondition;
+        pageEntity.tClass = tClass;
+        return pageEntity;
+    }
+
+    public PageEntity setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+        return this;
+    }
+
+
+    public PageEntity setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+        return this;
+    }
+
+    public PageEntity setTotalCount(long totalCount) {
+        this.totalCount = totalCount;
+        this.pageCount = totalCount % this.pageSize == 0 ? totalCount / this.pageSize : totalCount / this.pageSize + 1;
+        this.hasNext = this.totalCount > this.pageSize * this.pageNumber;
+        return this;
+    }
+
+    public PageEntity addSort(Sort sort) {
+        this.sorts.add(sort);
+        return this;
+    }
+
+    public PageEntity setList(ArrayList<T> list) {
+        this.list = list;
+        return this;
+    }
+
+    private int pageSize = 20;
+    private int pageNumber = 1;
+    private long totalCount = 0;
+    private long pageCount = 0;
+    private Boolean hasNext = false;
+    private Object queryCondition;
+    private Class<T> tClass;
+    private List<Sort> sorts = new ArrayList<>();
+
+    private List<T> list = new ArrayList<>();
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public long getTotalCount() {
+        return totalCount;
+    }
+
+    public Object getQueryCondition() {
+        return queryCondition;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public Boolean getHasNext() {
+        return hasNext;
+    }
+
+    public List<Sort> getSorts() {
+        return sorts;
+    }
+
+    public Class<T> getTClass() {
+        return tClass;
+    }
+
+    public long getPageCount() {
+        return pageCount;
+    }
+}
--- fast-toolkit3d/src/test/java/db/mongodb/MongodbManagerTest.java
+++ fast-toolkit3d/src/test/java/db/mongodb/MongodbManagerTest.java
@@ -1,6 +1,9 @@
 package db.mongodb;
 
 import com.thankjava.toolkit3d.fastjson.FastJson;
+import com.thankjava.toolkit3d.vo.db.PageEntity;
+import com.thankjava.toolkit3d.vo.db.Sort;
+import com.thankjava.toolkit3d.vo.db.SortType;
 import db.mongodb.po.Test;
 import org.bson.Document;
 import com.thankjava.toolkit.radom.Sequence;
@@ -14,17 +17,30 @@ import java.util.Date;
 public class MongodbManagerTest {
 
 
-
     public static void main(String[] args) {
         MongoDBManager mongoDBManager = MongoDriverManager.getSingleton();
 //        MongoDBManager mongoDBManager = MongoDriverManager.getSingleton("/Users/acexy/Downloads/mongodb.properties");
-        Test test = new Test();
-        test.setTime(new Date().getTime());
-        test.setValue("1");
-        System.out.println(mongoDBManager.insertOne("test", test));
 
 
-//        test.setValue("美国");
+//        int index = 0;
+//        for (int i = 0; i < 100; i++) {
+//            Test test = new Test();
+//            test.setTime(new Date().getTime());
+//            test.setValue(String.valueOf(i));
+//            System.out.println(mongoDBManager.insertOne("test", test));
+//        }
+
+//        test.setValue("10");
 //        System.out.println(mongoDBManager.updateOneByObjectId("test",test,"5a24f854eba60917d0191f8f"));
+
+        PageEntity<Test> pageEntity = PageEntity.newPageEntity(Test.class, new Test());
+        pageEntity
+                .addSort(new Sort("value", SortType.desc))
+                .addSort(new Sort("create_time", SortType.asc))
+                .setPageNumber(5)
+                .setPageSize(5)
+        ;
+        mongoDBManager.findByPage("test", pageEntity);
+        System.out.println(FastJson.toJSONString(pageEntity));
     }
 }
