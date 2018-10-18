27d37f6abb0b5e833c6a1d636105dc051695486a
Acexy <acexy@thankjava.com>
2018-10-18 09:31:47 +0800

    调整3des加密

--- fast-toolkit/src/main/java/com/thankjava/toolkit/core/encrypit/ThreeDES.java
+++ fast-toolkit/src/main/java/com/thankjava/toolkit/core/encrypit/ThreeDES.java
@@ -18,7 +18,17 @@ import java.security.NoSuchAlgorithmException;
 public final class ThreeDES {
 
 
-    private ThreeDES() {
+    private String priKey;
+
+    /**
+     * 32位字符对称加密密钥
+     * @param priKey
+     */
+    public ThreeDES(String priKey) {
+        if (priKey == null || priKey.length() != 32) {
+            throw new IllegalArgumentException("priKey can not be null and length will be 32");
+        }
+        this.priKey = priKey;
     }
 
     private static Cipher cipher;
@@ -33,35 +43,30 @@ public final class ThreeDES {
         }
     }
 
-    public static String encryptStr(String key, String encryptStr) {
-        key += "0000000000000000";
-        key = key.substring(0, 16);
-        String tempKey = diverse(KEY, key);
+    public String encrypt(String pubKey, String content) {
+        pubKey += "0000000000000000";
+        pubKey = pubKey.substring(0, 16);
+        String tempKey = diverse(priKey, pubKey);
 
-        String result = new String(hex(encryptStr.getBytes()));
-        result += "80";
-        while (result.length() % 16 != 0) {
-            result += "0";
+        StringBuilder stringBuilder = new StringBuilder(new String(hex(content.getBytes())));
+        stringBuilder.append("80");
+        while (stringBuilder.length() % 16 != 0) {
+            stringBuilder.append("0");
         }
-        return encrypt(tempKey, result);
+        return encryptStr(tempKey, stringBuilder.toString());
     }
 
-    public static String decodeStr(String key, String decodeStr) {
-        key += "0000000000000000";
-        key = key.substring(0, 16);
-        String tempKey = diverse(KEY, key);
-        String result = deEncrypt(tempKey, decodeStr);
+    public String decrypt(String pubKey, String decodeStr) {
+        pubKey += "0000000000000000";
+        pubKey = pubKey.substring(0, 16);
+        String tempKey = diverse(priKey, pubKey);
+        String result = deEncryptStr(tempKey, decodeStr);
         if (result.length() % 16 == 0) {
-            int dex = result.lastIndexOf("80");
-            String cutStr = result.substring(0, dex);
-            String resultStr = new String(unhexba(cutStr));
-            return resultStr;
+            return new String(unhexba(result.substring(0, result.lastIndexOf("80"))));
         } else {
             return "";
         }
     }
-
-    private static final String KEY = "0547CD5B408B47ACBC95F94B9CB88879";
 
     private static String diverse(String dkey, String random) {
         byte[] rand = unhexba(random);
@@ -72,30 +77,39 @@ public final class ThreeDES {
         return hex(tkey);
     }
 
-    private static String encrypt(String key, String data) {
+    private static String encryptStr(String key, String data) {
         return hex(do3des(unhexba(data), unhexba(key)));
     }
 
-    private static String deEncrypt(String key, String data) {
+    private static String deEncryptStr(String key, String data) {
         return hex(do3desun(unhexba(data), unhexba(key)));
     }
 
 
-    private static String[] byteHexTable = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B",
-            "0C", "0D", "0E", "0F", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "1A", "1B", "1C", "1D",
-            "1E", "1F", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "2A", "2B", "2C", "2D", "2E", "2F",
-            "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3A", "3B", "3C", "3D", "3E", "3F", "40", "41",
-            "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53",
-            "54", "55", "56", "57", "58", "59", "5A", "5B", "5C", "5D", "5E", "5F", "60", "61", "62", "63", "64", "65",
-            "66", "67", "68", "69", "6A", "6B", "6C", "6D", "6E", "6F", "70", "71", "72", "73", "74", "75", "76", "77",
-            "78", "79", "7A", "7B", "7C", "7D", "7E", "7F", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
-            "8A", "8B", "8C", "8D", "8E", "8F", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9A", "9B",
-            "9C", "9D", "9E", "9F", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "AA", "AB", "AC", "AD",
-            "AE", "AF", "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BA", "BB", "BC", "BD", "BE", "BF",
-            "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CA", "CB", "CC", "CD", "CE", "CF", "D0", "D1",
-            "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DA", "DB", "DC", "DD", "DE", "DF", "E0", "E1", "E2", "E3",
-            "E4", "E5", "E6", "E7", "E8", "E9", "EA", "EB", "EC", "ED", "EE", "EF", "F0", "F1", "F2", "F3", "F4", "F5",
-            "F6", "F7", "F8", "F9", "FA", "FB", "FC", "FD", "FE", "FF",};
+    private static final String[] byteHexTable = {
+            "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B",
+            "0C", "0D", "0E", "0F", "10", "11", "12", "13", "14", "15", "16", "17",
+            "18", "19", "1A", "1B", "1C", "1D", "1E", "1F", "20", "21", "22", "23",
+            "24", "25", "26", "27", "28", "29", "2A", "2B", "2C", "2D", "2E", "2F",
+            "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3A", "3B",
+            "3C", "3D", "3E", "3F", "40", "41", "42", "43", "44", "45", "46", "47",
+            "48", "49", "4A", "4B", "4C", "4D", "4E", "4F", "50", "51", "52", "53",
+            "54", "55", "56", "57", "58", "59", "5A", "5B", "5C", "5D", "5E", "5F",
+            "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "6A", "6B",
+            "6C", "6D", "6E", "6F", "70", "71", "72", "73", "74", "75", "76", "77",
+            "78", "79", "7A", "7B", "7C", "7D", "7E", "7F", "80", "81", "82", "83",
+            "84", "85", "86", "87", "88", "89", "8A", "8B", "8C", "8D", "8E", "8F",
+            "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9A", "9B",
+            "9C", "9D", "9E", "9F", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7",
+            "A8", "A9", "AA", "AB", "AC", "AD", "AE", "AF", "B0", "B1", "B2", "B3",
+            "B4", "B5", "B6", "B7", "B8", "B9", "BA", "BB", "BC", "BD", "BE", "BF",
+            "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CA", "CB",
+            "CC", "CD", "CE", "CF", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7",
+            "D8", "D9", "DA", "DB", "DC", "DD", "DE", "DF", "E0", "E1", "E2", "E3",
+            "E4", "E5", "E6", "E7", "E8", "E9", "EA", "EB", "EC", "ED", "EE", "EF",
+            "F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "FA", "FB",
+            "FC", "FD", "FE", "FF"
+    };
 
     private static String hex(byte[] dst) {
         StringBuffer sb = new StringBuffer();
--- fast-toolkit/src/test/java/encrypit/ThreeDESTest.java
+++ fast-toolkit/src/test/java/encrypit/ThreeDESTest.java
@@ -6,10 +6,12 @@ public class ThreeDESTest {
 
     public static void main(String[] args) {
 
-        String priKey = "a 3des's pri key";
-        String cipher = ThreeDES.encryptStr(priKey, "对称加密 3des");
+        ThreeDES threeDES = new ThreeDES("0547CD5B468B47ACBC95F94B9CB88879");
 
+        String pubKey = "PUB KEY";
+
+        String cipher = threeDES.encrypt(pubKey, "对称加密 3des");
         System.out.println("通过key加密后的密文：" + cipher);
-        System.out.println("通过key解密后的原文：" + ThreeDES.decodeStr(priKey, cipher));
+        System.out.println("通过key解密后的原文：" + threeDES.decrypt(pubKey, cipher));
     }
 }
