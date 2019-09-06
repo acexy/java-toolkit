59fccd6171e5215acbf4c8af14d92a83f0cd2137
Acexy <acexy@thankjava.com>
2019-09-06 11:36:41 +0800

    升级部分推荐依赖版本

--- fast-toolkit3d/src/test/java/mail/javax/MailUtilTest.java
+++ fast-toolkit3d/src/test/java/mail/javax/MailUtilTest.java
@@ -11,7 +11,7 @@ import com.thankjava.toolkit3d.bean.mail.MailContent;
 public class MailUtilTest {
 
 	public static void main(String[] args) throws MessagingException, UnsupportedEncodingException {
-		
+
 		MailContent mailContent = new MailContent(
 				MailService.ALIYUN, //选择发送邮件的供应商服务 --> 可以通过另外一个构造函数手动指定
 				"acexy@thankjava.com",//对方收件人邮箱
@@ -20,7 +20,7 @@ public class MailUtilTest {
 				"来自java发送的测试邮件",
 				"来着Fast-Toolkit3d发送的电子邮件"//邮件内容(支持标准邮件HTML结构)
 		);
-		
+
 //		MailUtil.sendMail(mailContent,new File("F:/Download/Browser/rootCA.crt"));//发送带附件的邮件
 		MailUtil.sendMail(mailContent);//发送不带附件的邮件
 	}
