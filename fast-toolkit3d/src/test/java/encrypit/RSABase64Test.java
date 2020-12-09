1c12f4ac4d0c32aae89ae12c2c35e6196076e0e7
Acexy <acexy@acexy.cn>
2020-12-09 18:55:43 +0800

    调整RAS测试

--- fast-toolkit3d/src/test/java/encrypit/RSABase64Test.java
+++ fast-toolkit3d/src/test/java/encrypit/RSABase64Test.java
@@ -13,39 +13,35 @@ public class RSABase64Test {
         String pi;
         String base64Cipher;
         String text;
-//        //获取1024位公密钥对
-//        RSAKeyString rsaKeyString = RSAWithBase64.keyGen(512);
-//        //公钥 base64
-//
-//        String text = "RSA非对称加密";
-//
-//        pk = rsaKeyString.getPublicKey();
-//        pi = rsaKeyString.getPrivateKey();
-//
-//        System.out.println(pi);
-//        System.out.println(rsaKeyString.getPrivatePem());
-//        System.out.println(pk);
-//        System.out.println(rsaKeyString.getPublicPem());
+        //获取1024位公密钥对
+        RSAKeyString rsaKeyString = RSAWithBase64.keyGen(1024);
+        //公钥 base64
 
-        pk = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKGcmle4bY5xTyd5dXu5koPB58xAMPEXF/adVyzBKku1Vk5umPGwGKxSRrxfdAkuLg4is+pP+SOWKNCCk0lI5C8CAwEAAQ==";
-        pi = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAoZyaV7htjnFPJ3l1e7mSg8HnzEAw8RcX9p1XLMEqS7VWTm6Y8bAYrFJGvF90CS4uDiKz6k/5I5Yo0IKTSUjkLwIDAQABAkA55zshfNG7/HsIwKrINpf5Hytm8gTXDJgoYv1N6rO1xeFlDq7gpq5tlFhL3gWFKBRjRZ5lg1UobBwtS0zhVx2RAiEA92EX1RbPotyA5qkrsB8OlKj+gSBFQuYIrA+tu2fCLVkCIQCnPl1tWfikFDCC4lhOVz1FCgTQXFKd89ai14k2EixExwIhAN9N9ux0zfPOplERDo3KSygosKyxC7SK3/MpqAMOc3rpAiBR5oqeKHdI8tIm5m1UQAizLIra9rtGusibim2ZWyDutQIhAPBGlykRghBjYNVfQsCyb+dVulFDVv0EuXz679HHHR8k";
+        text = "RSA非对称加密";
+
+        pk = rsaKeyString.getPublicKey();
+        pi = rsaKeyString.getPrivateKey();
+
+        System.out.println(pi);
+        System.out.println(rsaKeyString.getPrivatePem());
+        System.out.println(pk);
+        System.out.println(rsaKeyString.getPublicPem());
+
+//        pk = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKGcmle4bY5xTyd5dXu5koPB58xAMPEXF/adVyzBKku1Vk5umPGwGKxSRrxfdAkuLg4is+pP+SOWKNCCk0lI5C8CAwEAAQ==";
+//        pi = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAoZyaV7htjnFPJ3l1e7mSg8HnzEAw8RcX9p1XLMEqS7VWTm6Y8bAYrFJGvF90CS4uDiKz6k/5I5Yo0IKTSUjkLwIDAQABAkA55zshfNG7/HsIwKrINpf5Hytm8gTXDJgoYv1N6rO1xeFlDq7gpq5tlFhL3gWFKBRjRZ5lg1UobBwtS0zhVx2RAiEA92EX1RbPotyA5qkrsB8OlKj+gSBFQuYIrA+tu2fCLVkCIQCnPl1tWfikFDCC4lhOVz1FCgTQXFKd89ai14k2EixExwIhAN9N9ux0zfPOplERDo3KSygosKyxC7SK3/MpqAMOc3rpAiBR5oqeKHdI8tIm5m1UQAizLIra9rtGusibim2ZWyDutQIhAPBGlykRghBjYNVfQsCyb+dVulFDVv0EuXz679HHHR8k";
 //
 //        // 进行公钥加密
-//        text = "RSA测试";
-//        base64Cipher = RSAWithBase64.encrypt(text, pk);
-//        System.out.println("加密后的base64密文：" + base64Cipher);
-
+        text = "RSA测试";
+        base64Cipher = RSAWithBase64.encrypt(text, pk, RSAEncryptAlgorithm.RSA_ECB_PKCS1Padding);
+        System.out.println("加密后的base64密文：" + base64Cipher);
 
         // 私钥解密
-//        base64Cipher = "VgmM53Wiu7eKzZleY8vMvSmTtC+CrBYSTmwvBO/8a/1r3Q9C4YEpjsDyJvTFpQgqwic9TLirw9YxrQhJ5PdfgA==";
-//        System.out.println("解密的内容：" + RSAWithBase64.decrypt(base64Cipher, pi, RSAEncryptAlgorithm.RSA_ECB_OAEPWithSHA_1AndMGF1Padding));
+        System.out.println("解密的内容：" + RSAWithBase64.decrypt(base64Cipher, pi,RSAEncryptAlgorithm.RSA_ECB_PKCS1Padding));
 //
 //
-//        base64Cipher = RSAWithBase64.sign(text, pi);
-//        System.out.println("加签base64签文：" + base64Cipher);
+        base64Cipher = RSAWithBase64.sign(text, pi,RSASignAlgorithm.SHA256withRSA);
+        System.out.println("加签base64签文：" + base64Cipher);
 
-        text = "RSA测试";
-        base64Cipher = "guMXTdwCylOWp8avMdrjuNAqW7xaB7+Idzf1ExWs4XIYkg/a1CXsWs6hfEYii89cCFwBkd34PG7Mpc4Mi0h/BQ==";
         System.out.println("验签：" + RSAWithBase64.verify(text, base64Cipher, pk, RSASignAlgorithm.SHA256withRSA));
 
     }
